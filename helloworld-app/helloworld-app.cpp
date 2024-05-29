29e4eba40d859c50eda0027c4532e5d61b4c61ac
whitequill <5000697+whitequill@users.noreply.github.com>
2024-05-29 02:03:01 -0400

    Create helloworld-app.cpp

---
+++ helloworld-app/helloworld-app.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{   
+    hello();
+    std::cin.get();
+    pwsh();
+    std::cin.get();
+    return 0;
+}
