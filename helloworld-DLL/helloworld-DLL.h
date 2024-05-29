a9f46e585be6c35389d7e5fb379342b2baea7573
whitequill <5000697+whitequill@users.noreply.github.com>
2024-05-29 02:05:38 -0400

    Add files via upload

---
+++ helloworld-DLL/helloworld-DLL.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef HELLOWORLDDLL_EXPORTS
+#define HELLOWORLDDLL_API __declspec(dllexport)
+#else
+#define HELLOWORLDDLL_API __declspec(dllimport)
+#endif
+
+extern "C" HELLOWORLDDLL_API void hello();
+extern "C" HELLOWORLDDLL_API int pwsh();
+
